eaf2cbbfb64186b0f00e7c566fb2557b5f4f9f15
EMRE13333 <101513732+EMRE13333@users.noreply.github.com>
2022-10-09 12:53:37 +0300

    Delete Games directory

--- Games/9015014224.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitzoon/Rogue-Hub/main/Games/6403373529.lua", true))()
--- Games/9426795465.lua
+++
@@ -1,9 +0,0 @@
-if getgenv().Rogue_AlreadyLoaded ~= nil then error("Rogue Hub was already found running or you have other scripts executed!") return else getgenv().Rogue_AlreadyLoaded = 0 end
-
-if game.PlaceId ~= 9426795465 then return end
-
-local teleportFunc = queueonteleport or queue_on_teleport or syn and syn.queue_on_teleport
-
-if teleportFunc then
-    teleportFunc([[loadstring(game:HttpGet("https://raw.githubusercontent.com/Kitzoon/Rogue-Hub/main/Main.lua", true))()]])
-end
